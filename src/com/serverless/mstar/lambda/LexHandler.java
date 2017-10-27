022192889f64ea1e74ee7db3bbc170db4ab90ea4
Kshitij Gupta <kshitijgupta@Kshitijs-MacBook-Pro.local>
2017-10-27 14:38:17 -0400

    ABhay's final change

--- src/com/serverless/mstar/lambda/LexHandler.java
+++ src/com/serverless/mstar/lambda/LexHandler.java
@@ -1,67 +1,44 @@
 package com.serverless.mstar.lambda;
 
-import java.util.Map;
+import java.io.IOException;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.LexEvent;
-import com.amazonaws.services.lambda.runtime.events.LexEvent.CurrentIntent;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.serverless.mstar.domain.ExchangeResult;
-import com.serverless.mstar.domain.Exchanges;
-import com.serverless.mstar.lambda.response.DialogAction;
-import com.serverless.mstar.lambda.response.LexResponse;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.serverless.mstar.lambda.intent.processors.IntentMap;
+import com.serverless.mstar.lambda.response.EIDialogAction;
+import com.serverless.mstar.lambda.response.EIResponse;
 import com.serverless.mstar.lambda.response.Message;
-import com.serverless.mstar.rest.service.XigniteService;
 
-public class LexHandler implements RequestHandler<LexEvent, Object> {
+
+public class LexHandler implements RequestHandler<LexEvent,Object>{
 
 	@Override
 	public Object handleRequest(LexEvent input, Context context) {
-		// TODO Auto-generated method stub
-
-		LexResponse lex = null;
+		Object resp=null;
+		System.out.println("from request Handler");
+		System.out.println("Intent is "+input.getCurrentIntent());
+		String intentName=input.getCurrentIntent().getName();
 		try {
-			System.out.println("from request Handler");
-			System.out.println("Intent is " + input.getCurrentIntent());
-
-			CurrentIntent ci = input.getCurrentIntent();
-
-			Map<String, String> slots = ci.getSlots();
-
-			// ExchangeResult er= new XigniteService().getExchanges();
-
-			StringBuilder sb = new StringBuilder();
-
-			String resp = new XigniteService().getExchangesAsStr();
-			ObjectMapper mapper = new ObjectMapper();
-
-			ExchangeResult er = mapper.readValue(resp, ExchangeResult.class);
-
-			System.out.println("Outcome is " + resp);
-
-			for (Exchanges e : er.getExchangeDescriptions()) {
-				sb.append(e.getMarket()).append(" ");
-			}
-
-			DialogAction dialogAction = new DialogAction("Close", "Fulfilled", new Message("PlainText",
-					"Response from Lambda slot1 is " + slots.get("FlowerType") + "Exchanes are " + sb.toString()));
-
-			lex = new LexResponse(dialogAction, input.getSessionAttributes());
-			// return response;
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
+			if(IntentMap.containsIntent(intentName))
+			resp= IntentMap
+			               .getProcessor(intentName)
+			               .process(input);
+			else
+				resp=new EIResponse(new EIDialogAction("ElicitIntent", new Message("PlainText","I am sorry I did not get you")));
+				
+		} catch (JsonParseException e) {
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		return lex;
+	
+		return resp;
 	}
-
 }
 
-/*
- * try { response= new ObjectMapper().writeValueAsString(new
- * LexResponse(dialogAction, input.getSessionAttributes()));
- * System.out.println("from Handler ======");
- * System.out.println("response is "+response); } catch (JsonProcessingException
- * e) { // TODO Auto-generated catch block e.printStackTrace(); }
- */
+	
