0c7938e0ace3105f0417a4ee3931fd3966a7c2df
Kshitij Gupta <kshitijgupta@Kshitijs-MacBook-Pro.local>
2017-10-27 08:04:38 -0400

    including Pratik's change in service

--- src/com/trp/mstar/rest/service/XigniteService.java
+++ src/com/trp/mstar/rest/service/XigniteService.java
@@ -1,9 +1,13 @@
 package com.trp.mstar.rest.service;
 
+import java.io.IOException;
+
 import org.springframework.web.client.RestTemplate;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.trp.mstar.domain.BaseAPIDomainObject;
 import com.trp.mstar.domain.ExchangeResult;
+import com.trp.mstar.domain.XCompanyRecommendations;
 import com.trp.mstar.domain.globalnews.GlobalNewsListSectors;
 import com.trp.mstar.domain.globalnews.GlobalNewsTodaysMarketHeadlines;
 
@@ -24,12 +28,6 @@ public class XigniteService {
 		
 	}
 	
-	//Global News-  list Sectors - GlobalNewsListSectors
-	public BaseAPIDomainObject getGlobalNewsListSectors(){
-		return this.restTemplate.getForObject("https://globalnews.xignite.com/xGlobalNews.json/ListSectors?&_token=AE4A02E0271A4E77B78B314AEE9A132D",GlobalNewsListSectors.class);
-		
-		
-	}
 	
 	//Global News-  list Sectors - String
 	public String getGlobalNewsListSectorsAsStr(){
@@ -38,12 +36,6 @@ public class XigniteService {
 		
 	}
 	
-	//Global News-  Today's Market Headlines - GlobalNewsTodaysMarketHeadlines
-		public GlobalNewsTodaysMarketHeadlines getGlobalNewsTodaysMarketHeadlines(){
-			return this.restTemplate.getForObject("https://globalnews.xignite.com/xGlobalNews.json/GetTodaysMarketHeadlines?&_token=AE4A02E0271A4E77B78B314AEE9A132D",GlobalNewsTodaysMarketHeadlines.class);
-			
-			
-		}
 	
 	//Global News-  Today's Market Headlines - String
 	public String getGlobalNewsTodaysMarketHeadlinesAsStr(){
@@ -52,8 +44,37 @@ public class XigniteService {
 		
 	}
 	
-	public static void main(String s[]){
-		System.out.println("exchanges r "+new XigniteService().getGlobalNewsTodaysMarketHeadlinesAsStr());
+	
+	public String getEstimatesAsStr(){
+		return this.restTemplate.getForObject("https://factsetestimates.xignite.com/xFactSetEstimates.json/GetEstimates?IdentifierType=Symbol&Identifiers=MSFT,GOOG&EstimateTypes=EPS,Sales&ReportType=Annual&EstimateFiscalPeriod=2018FY&AsOfDate=10/25/2017&UpdatedSince=&_token=AE4A02E0271A4E77B78B314AEE9A132D",String.class);
 	}
+
+	public String getRecommendationsAsStr(){
+		return this.restTemplate.getForObject("https://factsetestimates.xignite.com/xFactSetEstimates.json/GetLatestRecommendationSummaries?IdentifierType=Symbol&Identifiers=MSFT,GOOG&UpdatedSince=&_token=AE4A02E0271A4E77B78B314AEE9A132D",String.class);
+	}
+
+
+	public static void main(String s[]){
+		XigniteService svc = new XigniteService();
+		String str = svc.getRecommendationsAsStr();
+		System.out.println("str is = \n"+str);
+		ObjectMapper mapper=new ObjectMapper();
+		//List<XCompanyEstimates> resList = mapper.readValue(str,List<XCompanyEstimates>.class>);
+		XCompanyRecommendations[] myObjects = null;
+		try {
+			myObjects = mapper.readValue(str, XCompanyRecommendations[].class);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if(myObjects!=null) {
+			System.out.println(myObjects.toString());
+		}
+		//List<XCompanyEstimates> myObjects = mapper.readValue(str, new TypeReference<List<XCompanyEstimates>>(){});
+}
+	
+	/*public static void main(String s[]){
+		System.out.println("exchanges r "+new XigniteService().getGlobalNewsTodaysMarketHeadlinesAsStr());
+	}*/
 	
 }
